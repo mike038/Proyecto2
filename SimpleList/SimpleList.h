557b314071f2d76d453875b5239a4bc50f9901a6
mike038 <miguel.aguilar038@gmail.com>
2020-05-11 17:06:06 -0500

    Update SimpleList.h

--- SimpleList/SimpleList.h
+++ SimpleList/SimpleList.h
@@ -16,11 +16,13 @@ typedef void (*destructor) (void*);
 typedef struct stSimpleList* simpleList;
 
 simpleList simpleList_create();
+void simpleList_print(simpleList);
 void simpleList_destroy(simpleList);
 int simpleList_size(simpleList);
 bool simpleList_add(simpleList, char*, Type);
 Type simpleList_get(simpleList, char*);
 bool simpleList_remove(simpleList, char*);
-int simpleList_isEmpty(simpleList);
+bool simpleList_isEmpty(simpleList);
+
 
 #endif /* SIMPLELIST_H_ */
